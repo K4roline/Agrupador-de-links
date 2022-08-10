e0d22a381ad70d3c7f3217bfca19eb0d4fd74fc9
Karoline Correa <ckaroline226@gmail.com>
2022-08-09 22:34:21 -0300

    Adicionando funcionalidades com JS

---
+++ script.js
@@ -0,0 +1,27 @@
+var ul = document.getElementById("Link")
+var li
+var itemId
+let item
+
+
+function SalvarLink() { 
+    if(document.getElementById("Link").value != {
+    
+    })
+
+}
+
+function RemoverLink() {
+
+}
+
+function CriarElemento(itemValue, itemId) {
+    let li = document.createElement("li")
+    li.setAttribute("index, itemId")
+    li.appendChild(document.createTextNode(itemValue))
+    return li
+}
+
+function RemoverLinkBtn() {
+
+}
