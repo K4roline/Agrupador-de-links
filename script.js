0103337ba09f9ad973a5e931f0b8966868a40c22
Karoline Correa <ckaroline226@gmail.com>
2022-08-26 23:41:10 -0300

    Trabalhando na funcionalidade de adiçao de links

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function SalvarLink () {
-    var lista = document.createElement("li")
-    var descricaoLink = document.getElementById("NovoLink").value
-    var Link = document.createTextNode(descricaoLink)
+    var li = document.createElement("li")
+    var inputValue = document.getElementById("NovoLink").value
+    var t = document.createTextNode(inputValue)
     li.appendChild(t)
 
     if (inputValue === '') {
@@ -12,16 +12,16 @@ function SalvarLink () {
 
     document.getElementById("NovoLink").value = ""
 
-    var span = document.createElement("salvarLink");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
+    var span = document.createElement("SPAN")
+    var txt = document.createTextNode("\u00D7")
+    span.className = "close"
+    span.appendChild(txt)
+    li.appendChild(span)
   
     for (i = 0; i < close.length; i++) {
       close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
+        var div = this.parentElement
+        div.style.display = "none"
       }
     }
   }
