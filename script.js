de23e2624c9716e59e1a67bfff542f4bf4eb7f47
Karoline Correa <ckaroline226@gmail.com>
2022-09-16 23:42:45 -0300

    Verificando a funcionalidade

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
 function add() {
-  let li = document.createElement('LI');
-  let input_value = document.form_main.task.value;
-  let input_text = document.createTextNode(input_value);
+  let li = document.createElement('LI')
+  let input_value = document.form_main.task.value
+  let input_text = document.createTextNode(input_value)
 
   li.appendChild(input_text);
-  document.querySelector('ul').appendChild(li);
-  document.form_main.task.value = "";
+  document.querySelector('ul').appendChild(li)
+  document.form_main.task.value = ""
 }
 
 
