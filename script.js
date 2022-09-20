f7a2cc85a6e2b16e220f127ce181cd74d1b789b5
Karoline Correa <ckaroline226@gmail.com>
2022-09-19 22:01:33 -0300

    Verificando as novas funcionalidades a serem incluidas

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ function add() {
   let input_value = document.form_main.task.value
   let input_text = document.createTextNode(input_value)
 
-  li.appendChild(input_text);
+  li.appendChild(input_text)
   document.querySelector('ul').appendChild(li)
   document.form_main.task.value = ""
 }
+
 
 
 
