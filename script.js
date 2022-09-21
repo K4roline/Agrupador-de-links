af9dc7546acca6978db9cb0695a82d0c7599cf40
Karoline Correa <ckaroline226@gmail.com>
2022-09-20 22:40:38 -0300

    Adicionando o campo de titulo do link

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 
 function add() {
-  let li = document.createElement('LI')
-  let input_value = document.form_main.task.value
-  let input_text = document.createTextNode(input_value)
+  var li = document.createElement('LI')
+  var input_value = document.form_main.tituloLink.value
+  var input_text = document.createTextNode(input_value)
 
   li.appendChild(input_text)
   document.querySelector('ul').appendChild(li)
   document.form_main.task.value = ""
+  
 }
-
-
 
 
