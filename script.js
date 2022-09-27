cbf905ad9251f9be39a000657b6bdeed4cd66cdc
Karoline Correa <ckaroline226@gmail.com>
2022-09-26 21:42:00 -0300

    Estudando a funcionalidade de adicao do titulo e link a lista

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-
-function add() {
-  var li = document.createElement('LI')
-  var input_value = document.form_main.tituloLink.value
-  var input_text = document.createTextNode(input_value)
-
-  li.appendChild(input_text)
-  document.querySelector('ul').appendChild(li)
-  document.form_main.task.value = ""
-  
+function salvar() {
+    var li = ""
+    var titulo = document.getElementById("tituloLink").value
+    var link = document.getElementById("Link").value
+    conteudo = titulo + link
+    document.getElementById("ul").value = li
 }
+
+
+
+
+// function add() {
+//   var li = document.createElement('LI')
+//   var input_value = document.form_main.tituloLink.value
+//   var input_text = document.createTextNode(input_value)
+
+//   li.appendChild(input_text)
+//   document.querySelector('ul').appendChild(li)
+//   document.form_main.task.value = ""
+  
+// }
+
+
+
+// Deixar o campo do link funcionando e alinhado com o titulo 
+// inserir o botao de excluir o link
+// inserir o acesso ao link com o click
 
 
 
