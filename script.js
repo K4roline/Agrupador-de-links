13ae0178a67989a9e2822e57175566f595641cae
Karoline Correa <ckaroline226@gmail.com>
2022-09-29 21:36:05 -0300

    Estudando a funcao de  adicao de links

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 
-function add() {
-  var li = document.createElement('LI')
-  var input_value = document.form_main.tituloLink.value
-  var input_text = document.createTextNode(input_value)
+// function add() {
+//   var li = document.createElement('LI')
+//   var input_value = document.form_main.tituloLink.value
+//   var input_text = document.createTextNode(input_value)
 
-  li.appendChild(input_text)
-  document.querySelector('ul').appendChild(li)
-  document.form_main.task.value = ""
+//   li.appendChild(input_text)
+//   document.querySelector('ul').appendChild(li)
+//   document.form_main.task.value = ""
   
-}
+// }
+
+
 
 
