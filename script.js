f91665c217f57762de0c190abb16abaff400db9e
Karoline Correa <ckaroline226@gmail.com>
2022-09-30 23:14:29 -0300

    Estudando a funcionalidade de adicao de conteudo de dois inputs a lista

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 
-// function add() {
-//   var li = document.createElement('LI')
-//   var input_value = document.form_main.tituloLink.value
-//   var input_text = document.createTextNode(input_value)
+function add() {
+  var li = document.createElement('LI')
+  var input_value = document.form_main.tituloLink.value
+  var input_text = document.createTextNode(input_value)
 
-//   li.appendChild(input_text)
-//   document.querySelector('ul').appendChild(li)
-//   document.form_main.task.value = ""
+  li.appendChild(input_text)
+  document.querySelector('ul').appendChild(li)
+  document.form_main.task.value = ""
   
-// }
+}
 
+
+// Deixar o campo do link funcionando e alinhado com o titulo 
+// inserir o botao de excluir o link 
+// inserir o acesso ao link com o click
 
 
 
