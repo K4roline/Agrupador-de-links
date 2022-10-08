103c2a7b36af7a69343a40068ab6ba87246821a8
Karoline Correa <ckaroline226@gmail.com>
2022-10-07 23:32:11 -0300

    Verificando o projeto

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 
 function add() {
   var li = document.createElement('LI')
-  var input_value = document.form_titulo.tituloLink.value
-  var input_value = document.form_link.Link.value
+  var input_value = document.getElementById(tituloLink).value
+  var input_value = document.getElementById(Link).value
   var input_text = document.createTextNode(input_value)
 
-  // li.appendChild(input_text)
-  // document.querySelector('ul').appendChild(li)
-  // document.form_titulo.task.value = ""
-  // document.form_link.task.value = ""
-  
+  li.appendChild(input_text)
+  document.querySelector('ul').appendChild(li)
+  document.form.tituloLink.value = ""
+  document.form.Link.value = ""
 }
+
+
+
+// function add() {
+//   var li = document.createElement('LI')
+//   var input_value = document.tituloLink.value
+//   var input_value = document.Link.value
+//   var input_text = document.createTextNode(input_value)
+
+//   li.appendChild(input_text)
+//   document.querySelector('ul').appendChild(li)
+//   document.form.tituloLink.value = ""
+//   document.form.Link.value = ""
+// }
+
 
 
 
