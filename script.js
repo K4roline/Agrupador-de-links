e61e16f5aef3de2f1a42117fc06d4d94804731e7
Karoline Correa <ckaroline226@gmail.com>
2022-10-10 22:03:55 -0300

    Verificando a funcionalidade

--- script.js
+++ script.js
@@ -1,29 +1,13 @@
 
 function add() {
-  var li = document.createElement('LI')
-  var input_value = document.getElementById(tituloLink).value
-  var input_value = document.getElementById(Link).value
+  var li = document.createElement('li')
+  var input_value = document.form.value
   var input_text = document.createTextNode(input_value)
 
   li.appendChild(input_text)
   document.querySelector('ul').appendChild(li)
-  document.form.tituloLink.value = ""
-  document.form.Link.value = ""
+  document.form.tituloLink.Link.value = ""
 }
-
-
-
-// function add() {
-//   var li = document.createElement('LI')
-//   var input_value = document.tituloLink.value
-//   var input_value = document.Link.value
-//   var input_text = document.createTextNode(input_value)
-
-//   li.appendChild(input_text)
-//   document.querySelector('ul').appendChild(li)
-//   document.form.tituloLink.value = ""
-//   document.form.Link.value = ""
-// }
 
 
 
