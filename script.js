5070ac0fe9a5297d5e171c7b95db577463af1cd9
Karoline Correa <ckaroline226@gmail.com>
2022-10-11 21:13:10 -0300

    Atualizando o projeto com o campo de adição de links

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 
 function add() {
   var li = document.createElement('li')
-  var input_value = document.form.value
+  // var input_value = document.form.tituloLink.value
+  var input_value = document.form.Link.value
   var input_text = document.createTextNode(input_value)
 
   li.appendChild(input_text)
   document.querySelector('ul').appendChild(li)
-  document.form.tituloLink.Link.value = ""
+  document.form.Link.value = ""
 }
+
+
+
 
 
 
