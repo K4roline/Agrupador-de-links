53ef2d85e3274ee4af966f91e4863ec1b11c5868
Karoline Correa <ckaroline226@gmail.com>
2022-10-13 23:36:02 -0300

    Estudando a adição do botão de exclusão dos links salvos

--- script.js
+++ script.js
@@ -8,9 +8,16 @@ function add() {
   li.appendChild(input_text)
   document.querySelector('ul').appendChild(li)
   document.form.Link.value = ""
-}
+  }
 
+  // var botao = document.createElement('button')
 
+  // botao.innerHTML = 'Excluir'
+
+  // campo_botao.addEventListener("click", function() {
+  // linha.parentNode.parentNode.removeChild(linha.parentNode)
+  // })
+  
 
 
 
