f6366759f4cfbcdaa3e8daf552fa52473db3d3e4
Karoline Correa <ckaroline226@gmail.com>
2022-11-01 22:10:44 -0300

    Finalizando o projeto

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function createCloseButton(li) {
   let span = document.createElement("SPAN")
   let txt = document.createTextNode("\u00D7")
 
-  span.className = "close";
+  span.className = "close"
   span.appendChild(txt)
   li.appendChild(span)
 
